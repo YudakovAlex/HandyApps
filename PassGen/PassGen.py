57758d9d260403c5256748bdcd003b49399bfc55
Ano nymous <yudakovalex@gmail.com>
2023-12-07 00:08:01 -0500

    Improved passgen

--- PassGen/PassGen.py
+++ PassGen/PassGen.py
@@ -1,38 +1,42 @@
 import string
-import random
+import secrets  # More secure than random for password generation
 
-# The pas function takes in a length parameter and optional boolean parameters 
-# to specify whether numbers, small letters, capital letters, and other symbols should be included.
-def get_password(length,
-        numbers=True,
-        small_letters=True,
-        capital_letter=True,
-        other_symbols=True
-        ):
+def get_password(length, include_numbers=True, include_small_letters=True,
+                 include_capitals=True, include_symbols=True):
+    """
+    Generate a random password with the given length and character inclusion settings.
+    """
+    if length <= 0 or not any([include_numbers, include_small_letters, include_capitals, include_symbols]):
+        raise ValueError("Invalid parameters for password generation.")
+
     chars = ""
-    # If a particular character type is set to True, add the corresponding characters to the character set.
-    if numbers:
+    if include_numbers:
         chars += string.digits
-    if small_letters:  
+    if include_small_letters:
         chars += string.ascii_lowercase
-    if capital_letter:  
+    if include_capitals:
         chars += string.ascii_uppercase
-    if other_symbols:  
+    if include_symbols:
         chars += string.punctuation
-    # Generate a list of random characters from the character set of specified length.
-    pw_list = [random.choice(chars) for _ in range(length)]
-    # Join the list of characters into a string password.
-    pw = ''.join(pw_list)
-    return pw
 
-# The generate_passwords function generates a list of passwords using the get_password function. 
-# It takes in two optional parameters: the number of passwords to generate (cnt) and the length of each password (lngth).
-def generate_passwords(cnt=20,lngth=9):
-    passwords = []
-    for _ in range(cnt):
-        passwords.append(get_password(lngth,other_symbols=True))
-    return passwords
+    while True:
+        password = ''.join(secrets.choice(chars) for _ in range(length))
+        if (include_numbers and not any(c in string.digits for c in password)):
+            continue
+        if (include_small_letters and not any(c in string.ascii_lowercase for c in password)):
+            continue
+        if (include_capitals and not any(c in string.ascii_uppercase for c in password)):
+            continue
+        if (include_symbols and not any(c in string.punctuation for c in password)):
+            continue
+        return password
 
-# Generate 20 passwords of length 9 with default settings and print them out.
-for p in generate_passwords():
-    print(p,"\n")
+def generate_passwords(count=20, length=12):
+    """
+    Generate a list of random passwords.
+    """
+    return [get_password(length) for _ in range(count)]
+
+# Generate and print passwords
+for password in generate_passwords():
+    print(password + "\n")
