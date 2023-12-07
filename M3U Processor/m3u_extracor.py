bf969644848265925010db99614f64c1145f5d20
Ano nymous <yudakovalex@gmail.com>
2023-12-07 00:27:54 -0500

    refactor m3uprocessor

--- M3U Processor/m3u_extracor.py
+++ M3U Processor/m3u_extracor.py
@@ -1,33 +1,47 @@
 import os
-from sys import argv
+import argparse
 from shutil import copy2
-from tqdm import tqdm
 
-CURR_DIR = os.getcwd()
+def extract(m3u_file, destination=None):
+    """
+    Extracts songs from an M3U playlist and copies them to the specified destination.
+    """
+    # Determine the directory and base name of the M3U file
+    m3u_dir = os.path.dirname(m3u_file)
+    m3u_base_name = os.path.splitext(os.path.basename(m3u_file))[0]
 
-def extract(m3u_file, destination):
-    with open(m3u_file, "r") as f:
-        m3u = f.read()
-    m3u = m3u.split("\n")
-    m3u = [s for s in m3u if s[:1] != "#" and s != ""]
-    songs = [s.split("/")[-1] for s in m3u]
-    m3u_dir = "/".join(m3u_file.split("/")[:-1])
-    if not(destination):
-        destination = CURR_DIR + "/Playlist"
-    if not os.path.exists(destination):
-        os.makedirs(destination)
-    for src, song in tqdm(zip(m3u,songs)):
-        copy2(m3u_dir + "/" + src, destination + "/" + song)
-        
+    # Default destination directory based on M3U file name
+    if not destination:
+        destination = os.path.join(m3u_dir, m3u_base_name)
+
+    # Create the destination directory if it doesn't exist
+    os.makedirs(destination, exist_ok=True)
+
+    with open(m3u_file, "r") as file:
+        for line in file:
+            if not line.startswith('#') and line.strip():
+                # Extract the song name and construct source and destination paths
+                song = os.path.basename(line.strip())
+                src = line.strip()
+                dest = os.path.join(destination, song)
+
+                # Copy the file
+                try:
+                    copy2(src, dest)
+                except IOError as e:
+                    print(f"Error copying {src}: {e}")
+    print("Done. Songs copied to " + destination)
+
+def parse_arguments():
+    """
+    Parses command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Extract songs from an M3U playlist.")
+    parser.add_argument("m3u_file", help="Path to the M3U file")
+    parser.add_argument("destination", nargs='?', default=None, help="Destination directory (optional)")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    m3u_f = None
-    dest = None
-    try:
-        m3u_f = argv[1]
-        if len(argv) >= 3: 
-            dest = argv[2]
-    except IndexError as e:
-        print("Please provide m3u file name as a parameter")
-        exit(1)
-    extract(m3u_f, dest)    
-
+    args = parse_arguments()
+    extract(args.m3u_file, args.destination)
+    
